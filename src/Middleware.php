1df8f43d7aec3b348cde94f6a2138c432bc54f5f
Maxime Rainville <maxime@silverstripe.com>
2022-12-08 15:30:24 +1300

    Add catch statement in middleware

--- src/Middleware.php
+++ src/Middleware.php
@@ -2,9 +2,12 @@
 
 namespace MaximeRainville\SilverstripeServerFingerprint;
 
+use Psr\Log\LoggerInterface;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Control\Middleware\HTTPMiddleware;
 use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\ORM\Connect\DatabaseException;
 
 class Middleware implements HTTPMiddleware
 {
@@ -18,16 +21,22 @@ class Middleware implements HTTPMiddleware
      */
     public function process(HTTPRequest $request, callable $delegate)
     {
-        if (Server::isReady()) {
 
-            $server = Server::singleton()->current();
+        try {
+            if (Server::isReady()) {
 
-            /** @var HTTPResponse $response */
-            $response = $delegate($request);
-            $response->addHeader('server-fingerprint', $server->getHeader());
+                $server = Server::singleton()->current();
 
-            return $response;
+                /** @var HTTPResponse $response */
+                $response = $delegate($request);
+                $response->addHeader('server-fingerprint', $server->getHeader());
+
+                return $response;
+            }
+        } catch (DatabaseException $ex) {
+            Injector::inst()->get(LoggerInterface::class)->warning('Could not finger print server because of Database exception. Database probably was not ready.');
         }
+
 
         return $delegate($request);
     }
